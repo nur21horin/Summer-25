ee38d144270a483e59da1d4c794c5aa367233ff0
nur21horin <nurm98371@gmail.com>
2025-05-06 10:55:40 +0600

    solution codechef 1350

--- test1350.java
+++ test1350.java
@@ -1,6 +1,25 @@
-public class test1350 {
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class test1350 {
     public static void main(String[] args) throws java.lang.Exception {
-        
-        
+
+        Scanner n=new Scanner(System.in);
+        int t=n.nextInt();
+        while(t-->0){
+           int U=n.nextInt();
+           int V=n.nextInt();
+           int A=n.nextInt();
+           int S=n.nextInt();
+           long minVelocitySquared = (long)U * U - 2L * A * S;
+           if(U<=V) System.out.println("Yes");
+          
+           else if ((long)V * V >= minVelocitySquared) {
+            System.out.println("Yes");
+       }
+           else System.out.println("No");
+
+        }
+        n.close();
     }
 }
